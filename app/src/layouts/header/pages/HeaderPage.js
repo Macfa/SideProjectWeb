5edf79b11e9b0199eb417dd2d0020dc4bbf20411
macfa <kombo6768@gmail.com>
2023-12-02 16:40:28 +0900

    modify README and add menu

--- app/src/layouts/header/pages/HeaderPage.js
+++ app/src/layouts/header/pages/HeaderPage.js
@@ -8,8 +8,9 @@ function HeaderPage() {
         <nav className='header_nav'>
           <ul className='header_list d-flex p-2 flex-row justify-content-between'>
             <li className='header_item'><NavLink className='header_target' to=''>Macfa</NavLink></li>
-            <li className='header_item'><NavLink className='header_target' to='work'>작업본</NavLink></li>
-            <li className='header_item'><NavLink className='header_target' to='intro'>양력</NavLink></li>
+            <li className='header_item'><NavLink className='header_target' to='plan'>Plan</NavLink></li>
+            <li className='header_item'><NavLink className='header_target' to='work'>Object</NavLink></li>
+            <li className='header_item'><NavLink className='header_target' to='intro'>Who's</NavLink></li>
           </ul>
         </nav>
     </header>
